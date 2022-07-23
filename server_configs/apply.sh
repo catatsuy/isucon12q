f1f871960d7e2f46a1b1471bdc2e2eeb7491d1ea
cyrill <1283323+t-cyrill@users.noreply.github.com>
2022-07-23 11:58:57 +0900

    server_configs deploy 微修正

--- server_configs/apply.sh
+++ server_configs/apply.sh
@@ -7,18 +7,19 @@ done
 
 # systemctl isuports
 for HOST in isu01 isu02 isu03; do
-  rsync ${HOST}/etc/systemd/system/isuports.service ${HOST}:/tmp/isuports.service
+  rsync -av ${HOST}/etc/systemd/system/isuports.service ${HOST}:/tmp/isuports.service
 
   ssh -T ${HOST} <<EOT
 sudo mv /tmp/isuports.service /etc/systemd/system/isuports.service
 sudo chown root. /etc/systemd/system/isuports.service
 sudo systemctl daemon-reload
+sudo systemctl restart isuports
 EOT
 done
 
 # nginx.conf
 for HOST in isu01 isu02 isu03; do
-  rsync ${HOST}/etc/nginx/ ${HOST}:/tmp/nginx/
+  rsync -av ${HOST}/etc/nginx/ ${HOST}:/tmp/nginx/
 
 ssh -T ${HOST} <<EOT
   sudo rsync -av /tmp/nginx/ /etc/nginx/
